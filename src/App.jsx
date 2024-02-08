3c045434a737129f68fe67dab62e56e8860d2880
gomdarachi <=>
2024-02-08 22:42:42 +0900

    setting: CSS Reset CDN

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,15 @@ import './App.css';
 import TextButton from './components/atoms/TextButton';
 
 function App() {
-  return <TextButton>개념 영상</TextButton>;
+  return (
+    <div className="mx-2 my-2">
+      <h1>H1</h1>
+      <TextButton color="white" shape="square">
+        중
+      </TextButton>
+      <TextButton color="gray">상세</TextButton>
+    </div>
+  );
 }
 
 export default App;
--- src/components/atoms/TextButton.jsx
+++ src/components/atoms/TextButton.jsx
@@ -1,26 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function TextButton({ color, children }) {
+function TextButton({ color, shape, children }) {
   if (color === 'white')
+    if (shape === 'long')
+      return (
+        <button
+          className="py-1 px-20 border-hpBlack border-[0.072rem] rounded-lg text-2xl font-semibold"
+          type="button"
+        >
+          {children}
+        </button>
+      );
+    else if (shape === 'square')
+      return (
+        <button
+          className="py-1 px-2 border-hpGray border-[0.072rem] rounded-lg text-2xl font-semibold"
+          type="button"
+        >
+          {children}
+        </button>
+      );
+  if (color === 'gray')
     return (
       <button
-        className="py-1 px-20 border-hpBlack border-[0.072rem] rounded-lg text-2xl font-semibold"
+        className="py-1 px-10 border-hpGray border-[0.072rem] rounded-full text-2xl font-semibold bg-hpLightGray"
         type="button"
       >
         {children}
       </button>
     );
-  if (color === 'gray') return <div>{children}</div>;
 }
 
 TextButton.propTypes = {
   children: PropTypes.string.isRequired,
   color: PropTypes.oneOf(['white', 'gray']),
+  shape: PropTypes.oneOf(['long', 'squre']),
 };
 
 TextButton.defaultProps = {
   color: 'white',
+  shape: 'long',
 };
 
 export default TextButton;
