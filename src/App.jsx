014bd3f0044235a87e20a4fc5c3d1bf86c66a696
gomdarachi <=>
2024-02-08 19:07:18 +0900

    setting: Sejong hospital Light 글꼴 로컬 설치

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ import './App.css';
 import TextButton from './components/atoms/TextButton';
 
 function App() {
-  return (
-    <div className="w-20 h-20 bg-hpRed">
-      <TextButton />
-    </div>
-  );
+  return <TextButton>개념 영상</TextButton>;
 }
 
 export default App;
--- src/components/atoms/TextButton.jsx
+++ src/components/atoms/TextButton.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function TextButton({ children }) {
-  if (children) return <div>{children}</div>;
+function TextButton({ color, children }) {
+  if (color === 'white')
+    return (
+      <button
+        className="py-1 px-20 border-hpBlack border-[0.072rem] rounded-lg text-2xl font-semibold"
+        type="button"
+      >
+        {children}
+      </button>
+    );
+  if (color === 'gray') return <div>{children}</div>;
 }
 
-export default TextButton;
-
-TextButton.prototype = {
-  children: PropTypes.node,
+TextButton.propTypes = {
+  children: PropTypes.string.isRequired,
+  color: PropTypes.oneOf(['white', 'gray']),
 };
+
+TextButton.defaultProps = {
+  color: 'white',
+};
+
+export default TextButton;
