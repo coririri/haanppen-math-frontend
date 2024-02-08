26f7cd55e6eafc182ed54cd8a4ad2d2597568382
gomdarachi <=>
2024-02-08 23:49:26 +0900

    feat: 텍스트 버튼 컴포넌트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import TextButton from './components/atoms/TextButton';
 function App() {
   return (
     <div className="mx-2 my-2">
-      <h1>H1</h1>
       <TextButton color="white" shape="square">
         중
+      </TextButton>
+      <TextButton color="white" shape="long">
+        개념 영상
       </TextButton>
       <TextButton color="gray">상세</TextButton>
     </div>
--- src/components/atoms/TextButton.jsx
+++ src/components/atoms/TextButton.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 function TextButton({ color, shape, children }) {
+  const [isClick, setIsClick] = useState(false);
+
+  const handleClick = () => {
+    setIsClick((prev) => !prev);
+  };
+
   if (color === 'white')
     if (shape === 'long')
       return (
         <button
-          className="py-1 px-20 border-hpBlack border-[0.072rem] rounded-lg text-2xl font-semibold"
+          className={`py-1 px-20 border-hpBlack border-[0.072rem] rounded-lg text-2xl font-semibold ${isClick ? 'bg-hpWhiteBlue' : 'bg-white'}`}
           type="button"
+          onClick={handleClick}
         >
           {children}
         </button>
@@ -15,8 +22,9 @@ function TextButton({ color, shape, children }) {
     else if (shape === 'square')
       return (
         <button
-          className="py-1 px-2 border-hpGray border-[0.072rem] rounded-lg text-2xl font-semibold"
+          className={`py-1 px-2 border-[0.072rem] rounded-lg text-2xl font-semibold ${isClick ? 'bg-hpWhiteBlue border-hpClickedWhiteBlue' : 'bg-white border-hpGray'}`}
           type="button"
+          onClick={handleClick}
         >
           {children}
         </button>
@@ -24,7 +32,7 @@ function TextButton({ color, shape, children }) {
   if (color === 'gray')
     return (
       <button
-        className="py-1 px-10 border-hpGray border-[0.072rem] rounded-full text-2xl font-semibold bg-hpLightGray"
+        className="py-1 px-10 border-hpGray border-[0.072rem] rounded-full text-2xl font-semibold bg-hpLightGray hover:bg-hpHoverLightGray"
         type="button"
       >
         {children}
@@ -35,7 +43,7 @@ function TextButton({ color, shape, children }) {
 TextButton.propTypes = {
   children: PropTypes.string.isRequired,
   color: PropTypes.oneOf(['white', 'gray']),
-  shape: PropTypes.oneOf(['long', 'squre']),
+  shape: PropTypes.oneOf(['long', 'square']),
 };
 
 TextButton.defaultProps = {
