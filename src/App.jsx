a2c89c37bac7b19e9595dc8f346766810452c6ce
gomdarachi <=>
2024-02-16 00:00:30 +0900

    feat: classList 컴포넌트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,13 @@
-import { BsPencilFill } from 'react-icons/bs';
-import { MdOndemandVideo } from 'react-icons/md';
-import IconButton from './components/atoms/IconButton';
+import ClassList from './components/atoms/ClassList';
 
 function App() {
   return (
     <div className="m-4">
-      <div className="mb-8">
-        <IconButton
-          bgColor="white"
-          text="글 쓰기"
-          icon={<BsPencilFill color="black" size="1rem" />}
-        />
-      </div>
-      <div className="mb-8">
-        <IconButton
-          bgColor="white"
-          text="완료"
-          icon={<BsPencilFill color="black" size="1rem" />}
-        />
-      </div>
-      <div className="mb-8">
-        <IconButton
-          bgColor="red"
-          text="영상 관리"
-          icon={<BsPencilFill color="white" size="1.4rem" />}
-        />
-      </div>
-      <IconButton
-        bgColor="blue"
-        text="영상 관리"
-        icon={<MdOndemandVideo color="white" size="1.8rem" />}
+      <ClassList
+        type="progress"
+        text="개념원리 중3-1 ~p.187 (이차함수의 그래프)"
       />
+      <ClassList type="homework" text="~p.187 까지" />
     </div>
   );
 }
