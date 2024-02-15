09e781c4f2a0eed957eed01e87cc7f3e29b1e74f
gomdarachi <=>
2024-02-16 01:26:33 +0900

    feat: DayClassList 컴포넌트 구현

--- src/components/atoms/IconButton.jsx
+++ src/components/atoms/IconButton.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 function IconButton({ bgColor, icon, text, handleClick }) {
   if (bgColor === 'white') {
     return (
-      <button type="button" onClick={handleClick}>
+      <button type="button" onClick={handleClick} className="h-9">
         <div className="inline-flex items-center font-bold border-[0.075rem] border-solid py-2 pl-4 pr-6 rounded-lg">
           {icon}
           <span className="ml-4 text-xs">{text}</span>
@@ -13,7 +13,7 @@ function IconButton({ bgColor, icon, text, handleClick }) {
   }
   if (bgColor === 'red') {
     return (
-      <button type="button" onClick={handleClick}>
+      <button type="button" onClick={handleClick} className="h-9">
         <div className="bg-hpLightRed inline-flex items-center font-bold border-[0.075rem] border-solid border-[#A50028] py-[0.2rem] px-3 rounded-lg">
           <div className="w-7">{icon}</div>
           <span className="ml-2 text-xl text-white font-sjBold">{text}</span>
@@ -23,7 +23,7 @@ function IconButton({ bgColor, icon, text, handleClick }) {
   }
   if (bgColor === 'blue') {
     return (
-      <button type="button" onClick={handleClick}>
+      <button type="button" onClick={handleClick} className="h-9">
         <div className="bg-hpLightBlue inline-flex items-center font-bold border-[0.075rem] border-solid border-[#008FE0] py-[0.2rem] px-3 rounded-lg">
           <div className="w-7">{icon}</div>
           <span className="ml-2 text-xl text-white font-sjBold">{text}</span>
