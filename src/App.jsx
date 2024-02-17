9a7fdb62248f495e00bcaf5ee26be25625ff5e0a
gomdarachi <=>
2024-02-17 14:24:09 +0900

    config: TextButton 크기 조정

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,15 @@
-import { BsPencilFill } from 'react-icons/bs';
-import { MdOndemandVideo } from 'react-icons/md';
-import IconButton from './components/atoms/IconButton';
+import TextButton from './components/atoms/TextButton';
 
 function App() {
   return (
-    <div className="m-4">
-      <IconButton
-        bgColor="white"
-        text="로그아웃"
-        icon={<BsPencilFill color="black" size="1.5rem" />}
-      />
-      <div className="mb-8">
-        <IconButton
-          bgColor="white"
-          text="완료"
-          icon={<BsPencilFill color="black" size="1.5rem" />}
-        />
-      </div>
-      <div className="mb-8">
-        <IconButton
-          bgColor="red"
-          text="영상 관리"
-          icon={<BsPencilFill color="white" size="1rem" />}
-        />
-      </div>
-      <IconButton
-        bgColor="blue"
-        text="영상 관리"
-        icon={<MdOndemandVideo color="white" size="1rem" />}
-      />
+    <div className="mx-2 my-2">
+      <TextButton color="white" shape="square">
+        중
+      </TextButton>
+      <TextButton color="white" shape="long">
+        개념 영상
+      </TextButton>
+      <TextButton color="gray">상세</TextButton>
     </div>
   );
 }
