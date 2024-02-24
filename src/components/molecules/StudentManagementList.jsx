8ac1fc9cae9b52c7797a7d2af25692e67dabb541
gomdarachi <=>
2024-02-24 14:38:52 +0900

    feat: studentManagementList 컴포넌트 구현

--- src/components/molecules/StudentManagementList.jsx
+++ src/components/molecules/StudentManagementList.jsx
@@ -1,25 +1,24 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 import TextButton from '../atoms/TextButton';
-import studentInformationShape from '../../types/studentInformation';
 import gradeTransform from '../../utils/gradeTransform';
 
-function StudentManagementList({
-  studentInformationArr,
-  setDeletedIndexArr,
-  index,
-}) {
+function StudentManagementList({ setDeletedIndexArr, index, checked }) {
+  const studentList = useSelector((state) => state.students);
+
   const [openModal, setOpenModal] = useState(false);
   const handleOpenModal = () => {
     setOpenModal((prev) => !prev);
   };
-  const { grade, name, phoneNumber } = studentInformationArr[index];
+  const { grade, name, phoneNumber } = studentList[index];
   return (
-    <div className="w-[42.875rem] h-[3.375rem] flex items-center justify-between">
-      <div className="ml-8">
+    <div className="w-[42.875rem] h-[3.375rem] flex items-center">
+      <div className="w-[7.5rem]">
         <input
           type="checkbox"
-          className="w-[1.125rem] h-[1.125rem] flex items-center border-hpLightkBlack border-solid border-[0.1rem]"
+          className="w-[1.125rem] h-[1.125rem] mx-auto flex items-center border-hpLightkBlack border-solid border-[0.1rem]"
+          checked={checked}
           onChange={() => {
             setDeletedIndexArr((prev) => {
               const newDeletedIndexArr = prev.slice();
@@ -29,18 +28,18 @@ function StudentManagementList({
           }}
         />
       </div>
-      <div>
+      <div className="w-[8rem] flex justify-center">
         <span className="text-xl text-hpBlack font-bold">
           {gradeTransform(grade)}
         </span>
       </div>
-      <div>
+      <div className="w-[11.375rem] flex justify-center">
         <span className="text-xl text-hpBlack font-bold">{name}</span>
       </div>
-      <div>
+      <div className="w-[16rem] flex justify-center">
         <span className="text-xl text-hpBlack font-bold">{phoneNumber}</span>
       </div>
-      <div className="mr-4">
+      <div className="w-[11rem] flex justify-center">
         <TextButton
           color="gray"
           shape="long"
@@ -54,11 +53,9 @@ function StudentManagementList({
 }
 
 StudentManagementList.propTypes = {
-  studentInformationArr: PropTypes.arrayOf(
-    PropTypes.shape(studentInformationShape),
-  ).isRequired,
   setDeletedIndexArr: PropTypes.func.isRequired,
   index: PropTypes.number.isRequired,
+  checked: PropTypes.bool.isRequired,
 };
 
 export default StudentManagementList;
---
+++ src/components/organisms/StudentManagementContainer.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import StudentManagementList from '../molecules/StudentManagementList';
+import { register } from '../../stores/slices/studentListSlice';
+
+function StudentManagementContainer() {
+  const studentList = useSelector((state) => state.students);
+  const dispatch = useDispatch();
+
+  const [deletedIndexArr, setDeletedIndexArr] = useState([]);
+  return (
+    <div className="m-10">
+      <div className="w-[42.875rem] h-[3.375rem] flex items-center">
+        <div className="w-[7.5rem]">
+          <input
+            type="checkbox"
+            className="w-[1.125rem] h-[1.125rem] mx-auto flex items-center border-hpLightkBlack border-solid border-[0.1rem]"
+            disabled
+          />
+        </div>
+        <div className="w-[8rem] flex justify-center">
+          <span className="text-xl text-hpGray font-bold">학년</span>
+        </div>
+        <div className="w-[11.375rem] flex justify-center">
+          <span className="text-xl text-hpGray font-bold">이름</span>
+        </div>
+        <div className="w-[16rem] flex justify-center">
+          <span className="text-xl text-hpGray font-bold">
+            학생 ID(전화 번호)
+          </span>
+        </div>
+        <div className="w-[11rem] flex justify-center text-hpGray font-bold">
+          수정
+        </div>
+      </div>
+      <div className="w-[45rem] h-[20.25rem] overflow-y-auto border-hpBlack border-solid border-y-[0.125rem]">
+        {deletedIndexArr.map((deleted, index) => (
+          <div
+            key={studentList[index].phoneNumber}
+            className="border-hpGray border-solid border-b-[0.1rem]"
+          >
+            <StudentManagementList
+              setDeletedIndexArr={setDeletedIndexArr}
+              index={index}
+              checked={deleted}
+            />
+          </div>
+        ))}
+      </div>
+      <div className="mt-4">
+        <button
+          type="button"
+          onClick={() => {
+            setDeletedIndexArr((prev) => [...prev, false]);
+            dispatch(
+              register({
+                grade: 11,
+                name: '조인애',
+                phoneNumber: '010-3433-0652',
+              }),
+            );
+          }}
+        >
+          학생 추가
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default StudentManagementContainer;
