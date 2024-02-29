0b3162b42de45885f14325038abc5ef598f8abde
gomdarachi <=>
2024-02-29 18:48:48 +0900

    feat: ClassDropdown Component 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,151 @@
-import TeacherManagementContainer from './components/organisms/TeacherManagementContainer';
+import { useState } from 'react';
+import ClassDropdown from './components/organisms/ClassDropdown';
 
 function App() {
+  const [classInformation, setGradeByStudent] = useState([
+    {
+      grade: 1,
+      num: 0,
+      students: [],
+    },
+    {
+      grade: 2,
+      num: 1,
+      students: ['김땡땡'],
+    },
+    {
+      grade: 3,
+      num: 3,
+      students: ['이땡땡', '선땡땡', '주땡땡'],
+    },
+    {
+      grade: 4,
+      num: 5,
+      students: ['후땡땡', '알땡땡', '쿠땡땡', '냥땡땡', '뿌뿌뿌'],
+    },
+    {
+      grade: 5,
+      num: 5,
+      students: ['후땡땡', '알땡땡', '쿠땡땡', '냥땡땡', '뿌뿌뿌'],
+    },
+    {
+      grade: 6,
+      num: 0,
+      students: [],
+    },
+    {
+      grade: 7,
+      num: 2,
+      students: ['냥땡땡', '뿌뿌뿌'],
+    },
+    {
+      grade: 8,
+      num: 0,
+      students: [],
+    },
+    {
+      grade: 9,
+      num: 0,
+      students: [],
+    },
+    {
+      grade: 10,
+      num: 0,
+      students: [],
+    },
+    {
+      grade: 11,
+      num: 0,
+      students: [],
+    },
+    {
+      grade: 12,
+      num: 0,
+      students: [],
+    },
+  ]);
+
+  const [newClassInformation, setNewGradeByStudent] = useState([
+    {
+      grade: 1,
+      num: 0,
+      students: [],
+    },
+    {
+      grade: 2,
+      num: 0,
+      students: [],
+    },
+    {
+      grade: 3,
+      num: 0,
+      students: [],
+    },
+    {
+      grade: 4,
+      num: 0,
+      students: [],
+    },
+    {
+      grade: 5,
+      num: 0,
+      students: [],
+    },
+    {
+      grade: 6,
+      num: 0,
+      students: [],
+    },
+    {
+      grade: 7,
+      num: 0,
+      students: [],
+    },
+    {
+      grade: 8,
+      num: 0,
+      students: [],
+    },
+    {
+      grade: 9,
+      num: 0,
+      students: [],
+    },
+    {
+      grade: 10,
+      num: 0,
+      students: [],
+    },
+    {
+      grade: 11,
+      num: 0,
+      students: [],
+    },
+    {
+      grade: 12,
+      num: 0,
+      students: [],
+    },
+  ]);
+
   return (
-    <div>
-      <TeacherManagementContainer />
+    <div className="m-4 flex ">
+      <ClassDropdown
+        gradeByStudent={classInformation}
+        setGradeByStudent={setGradeByStudent}
+        newClassInformation={newClassInformation}
+        setNewGradeByStudent={setNewGradeByStudent}
+        type="plus"
+      />
+      <div className="ml-4">
+        <ClassDropdown
+          gradeByStudent={classInformation}
+          setGradeByStudent={setGradeByStudent}
+          newClassInformation={newClassInformation}
+          setNewGradeByStudent={setNewGradeByStudent}
+          type="minus"
+        />
+      </div>
     </div>
   );
 }
--- src/components/atoms/TextButton.jsx
+++ src/components/atoms/TextButton.jsx
@@ -1,6 +1,14 @@
 import PropTypes from 'prop-types';
 
-function TextButton({ color, shape, text, isClick, handleClick, moreStyle }) {
+function TextButton({
+  color,
+  shape,
+  size,
+  text,
+  isClick,
+  handleClick,
+  moreStyle,
+}) {
   if (color === 'white')
     if (shape === 'long')
       return (
@@ -27,17 +35,54 @@ function TextButton({ color, shape, text, isClick, handleClick, moreStyle }) {
       <button
         className={`inline-block w-[6.25rem] h-[2rem] border-hpGray border-[0.072rem] rounded-full font-bold bg-hpLightGray hover:bg-hpHoverLightGray ${moreStyle}`}
         type="button"
+        onClick={handleClick}
       >
         <span className="text-xl">{text}</span>
       </button>
     );
+  if (shape === 'math') {
+    if (size === 'big') {
+      return (
+        <button
+          className={`inline-block w-[2.125rem] h-[2.125rem] border-hpWhiteBlue border-[0.072rem] rounded-full font-bold bg-hpWhiteBlue hover:border-hpClickedWhiteBlue ${moreStyle}`}
+          type="button"
+          onClick={handleClick}
+        >
+          <span className="text-3xl leading-[2.125rem]">{text}</span>
+        </button>
+      );
+    }
+    if (size === 'small') {
+      return (
+        <button
+          className={`inline-block w-[1.43rem] h-[1.43rem] border-hpWhiteBlue border-[0.072rem] rounded-full font-bold bg-hpWhiteBlue hover:border-hpClickedWhiteBlue ${moreStyle}`}
+          type="button"
+          onClick={handleClick}
+        >
+          <span className="text-xl leading-[1.43rem]">{text}</span>
+        </button>
+      );
+    }
+    if (size === 'tooSmall') {
+      return (
+        <button
+          className={`inline-block w-[1.2rem] h-[1.2rem] leading-[1.2rem] bg-hpBlack text-white hover:border-hpBlack border-hpLightBlack border-[0.072rem] rounded-full font-bold ${moreStyle}`}
+          type="button"
+          onClick={handleClick}
+        >
+          <span className="text-xl leading-[1.2rem]">{text}</span>
+        </button>
+      );
+    }
+  }
 }
 
 TextButton.propTypes = {
   text: PropTypes.string.isRequired,
   color: PropTypes.oneOf(['white', 'gray']),
-  shape: PropTypes.oneOf(['long', 'square']),
-  isClick: PropTypes.bool.isRequired,
+  shape: PropTypes.oneOf(['long', 'square', 'math']),
+  size: PropTypes.oneOf(['big', 'small', 'tooSmall']),
+  isClick: PropTypes.bool,
   handleClick: PropTypes.func.isRequired,
   moreStyle: PropTypes.string,
 };
@@ -46,6 +91,8 @@ TextButton.defaultProps = {
   color: 'white',
   shape: 'long',
   moreStyle: '',
+  size: 'small',
+  isClick: false,
 };
 
 export default TextButton;
