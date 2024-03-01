8777a54983c86cd2b4ca873b542935f59c501216
gomdarachi <=>
2024-03-01 17:55:49 +0900

    config: Input 컴포넌트 ref 삭제 - 직접적으로 input dom을 조작할 일이 없기 때문에 event로도 충분히 관리   가능

--- src/components/atoms/Input.jsx
+++ src/components/atoms/Input.jsx
@@ -1,19 +1,14 @@
 import PropTypes from 'prop-types';
 import { AiOutlineSearch } from 'react-icons/ai';
 
-function Input({
-  type,
-  inputRef,
-  placeholder,
-  handleClickSearch,
-  handleCheckbox,
-}) {
+function Input({ type, placeholder, handleChange, handleClick, value }) {
   if (type === 'singleLine') {
     return (
       <input
         className="w-full text-md font-cantarell font-bold rounded-lg border-[0.075rem] border-black bg-white px-2 py-[0.4rem] focus:outline-none"
         type="text"
-        ref={inputRef}
+        value={value}
+        onChange={handleChange}
       />
     );
   }
@@ -22,7 +17,8 @@ function Input({
       <div className="w-[20rem] h-[30rem]">
         <textarea
           className="w-full h-full text-md font-cantarell font-bold bg-white focus:outline-none"
-          ref={inputRef}
+          value={{ value }}
+          onChange={handleChange}
         />
       </div>
     );
@@ -33,10 +29,10 @@ function Input({
         <input
           className="w-full h-[3.5rem] text-2xl font-cantarell rounded-l-md border-y-[0.15rem] border-l-[0.15rem] border-[#DADADA] bg-white px-2 py-[0.4rem] focus:outline-none"
           type="text"
-          ref={inputRef}
+          value={value}
           placeholder={placeholder}
         />
-        <button type="button" aria-label="Search" onClick={handleClickSearch}>
+        <button type="button" aria-label="Search" onClick={handleChange}>
           <div className="w-[3.4rem] h-[3.5rem] flex items-center justify-center border-[0.15rem] border-solid border-[#DADADA] bg-[#DADADA]">
             <AiOutlineSearch size="3.3rem" color="#C1C1C1" />
           </div>
@@ -50,7 +46,7 @@ function Input({
         <input
           type="checkbox"
           className="w-[1.125rem] h-[1.125rem] mx-auto flex items-center border-hpLightkBlack border-solid border-[0.1rem]"
-          onChange={handleCheckbox}
+          onChange={handleClick}
         />
       </div>
     );
@@ -60,17 +56,17 @@ function Input({
 Input.propTypes = {
   type: PropTypes.oneOf(['singleLine', 'multiLine', 'search', 'checkbox'])
     .isRequired,
-  inputRef: PropTypes.shape({ current: PropTypes.instanceOf(Element) })
-    .isRequired,
   placeholder: PropTypes.string,
-  handleClickSearch: PropTypes.func,
-  handleCheckbox: PropTypes.func,
+  handleChange: PropTypes.func,
+  handleClick: PropTypes.func,
+  value: PropTypes.string,
 };
 
 Input.defaultProps = {
   placeholder: '',
-  handleClickSearch: null,
-  handleCheckbox: null,
+  handleChange: null,
+  handleClick: null,
+  value: null,
 };
 
 export default Input;
