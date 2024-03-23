812fd18fc7eae1b16bd06d6a476f165b5fc56ca8
gomdarachi <=>
2024-03-23 23:13:55 +0900

    feat: refreshToken을 받아오는 로직 구현 - api요청을 intercept하여 구현

--- src/apis/instance.js
+++ src/apis/instance.js
@@ -7,4 +7,26 @@ const instance = axios.create({
   timeout: 1000,
 });
 
+// 새로운 토큰을 가져오는 함수
+const fetchNewToken = async () => {
+  const response = await instance.post('/api/login/refresh');
+  const newToken = response.data.accessToken;
+  const { role } = response.data;
+  instance.defaults.headers.common.Authorization = newToken;
+  localStorage.setItem('role', role);
+  return newToken;
+};
+
+instance.interceptors.request.use(async () => {
+  // accessToken이 없거나 만료된 경우 새로운 토큰을 가져오는 로직
+  let token = instance.defaults.headers.common.Authorization;
+  if (!token) {
+    try {
+      token = await fetchNewToken();
+    } catch (error) {
+      console.error('토큰을 갱신하는 중 에러가 발생했습니다:', error);
+    }
+  }
+  instance.defaults.headers.common.Authorization = token; // 요청에 토큰 설정
+});
 export default instance;
