bb5072343a154c37a681a8d5ce8d75b8e874e1f3
gomdarachi <=>
2024-03-26 00:36:53 +0900

    feat: window.location.href 사용 해 라우팅
    intercept에서 refresh 요청 실패 시 라우팅
    useNavigation을 사용하지 못해서

--- src/apis/instance.js
+++ src/apis/instance.js
@@ -28,6 +28,10 @@ instance.interceptors.request.use(async (config) => {
     } catch (error) {
       console.error('토큰을 갱신하는 중 에러가 발생했습니다:', error);
       // 토큰 갱신에 실패한 경우 여기에 적절한 처리를 추가할 수 있습니다.
+      setTimeout(() => {
+        alert('로그인 페이지로 이동합니다');
+        window.location.href = '/login';
+      }, 3000);
     }
   }
   return config;
