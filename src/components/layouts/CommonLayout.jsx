7c10bb68eedc7b621de04a0696322f0b8e4d8d1a
gomdarachi <=>
2024-03-26 00:30:25 +0900

    feat: refreshLogin 요청 실패 대응

--- src/components/layouts/CommonLayout.jsx
+++ src/components/layouts/CommonLayout.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
-import { Outlet } from 'react-router-dom';
+import { useNavigate, Outlet } from 'react-router-dom';
 import instance, { refreshInstance } from '../../apis/instance';
 
 function CommonLayout() {
+  const navigate = useNavigate();
   useEffect(() => {
     const fetchData = async () => {
       const curToken = instance.defaults.headers.common.Authorization;
@@ -19,6 +20,10 @@ function CommonLayout() {
         } catch (error) {
           console.error('토큰을 갱신하는 중 에러가 발생했습니다:', error);
           // 토큰 갱신에 실패한 경우 여기에 적절한 처리를 추가할 수 있습니다.
+          setTimeout(() => {
+            alert('로그인 페이지로 이동합니다');
+            navigate('/login');
+          }, 3000);
         }
       }
     };
