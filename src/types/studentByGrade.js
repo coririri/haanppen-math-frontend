bf273282292afa7d2e6f01a0bdf90dfadf12f1e3
gomDarachi <87762581+coririri@users.noreply.github.com>
2024-03-28 02:24:43 +0900

    Revert "Feat: 헤더 구현"

--- src/types/studentByGrade.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types';
-
-const studentByGrade = {
-  grade: PropTypes.number.isRequired,
-  num: PropTypes.number.isRequired,
-  students: PropTypes.arrayOf(PropTypes.string).isRequired,
-};
-
-export default studentByGrade;
--- src/types/studentInformation.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types';
-
-const studentInformationShape = {
-  grade: PropTypes.number.isRequired,
-  name: PropTypes.string.isRequired,
-  phoneNumber: PropTypes.string.isRequired,
-};
-
-export default studentInformationShape;
