bd066b83fe49db32d7cd45b1917d4a10f69eb18f
gomdarachi <=>
2024-04-19 01:20:52 +0900

    config: Header, Navigation 미디어 쿼리 수정

--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -17,12 +17,43 @@ function Header() {
     roleTheme = '없음';
   }
 
+  if (role === 'STUDENT') {
+    return (
+      <div>
+        <header className="mx-auto w-[428px] h-[96px] flex justify-between items-center">
+          <button
+            type="button"
+            className="ml-4"
+            onClick={() => {
+              window.location.reload();
+            }}
+          >
+            <img src={logoImages} alt="한편의 수학 로고 이미지" />
+          </button>
+          <span className="text-hpDarkBlue text-3xl font-sjBold">
+            {roleTheme}
+          </span>
+          <Link to="/user-information" className="mr-4">
+            <div className="flex items-center h-[46px] rounded-l-[40px] rounded-r-[40px] border-solid border-[#E0E0E0] border-[1.5px]">
+              <div className="ml-2 mr-2 rounded-[15px] bg-hpGray flex items-center justify-center">
+                <BsFillPersonFill color="#FFF" size="23px" />
+              </div>
+              <div className="mr-4 text-center whitespace-nowrap overflow-y-hidden overflow-x-auto">
+                <span className="text-xl text-hpBlack">{userName}</span>
+              </div>
+            </div>
+          </Link>
+        </header>
+        <hr className="w-[428px] mx-auto bg-hpGray" />
+      </div>
+    );
+  }
   return (
-    <div>
-      <header className="mx-auto w-[428px] h-[96px] lg:w-[1440px] lg:h-[80px] flex justify-between items-center">
+    <div className="mx-auto">
+      <header className="mx-auto w-[1440px] h-[80px] flex justify-between items-center">
         <button
           type="button"
-          className="ml-4 lg:ml-64"
+          className="ml-64"
           onClick={() => {
             window.location.reload();
           }}
@@ -32,20 +63,18 @@ function Header() {
         <span className="text-hpDarkBlue text-3xl font-sjBold">
           {roleTheme}
         </span>
-        <Link to="/user-information" className="mr-4 lg:mr-[245px]">
-          <div className="flex items-center h-[46px] rounded-l-[40px] rounded-r-[40px] border-solid border border-[#E0E0E0] border-[1.5px]">
-            <div className="lg:w-[30px] lg:h-[30px] ml-2 mr-2 rounded-[15px] bg-hpGray flex items-center justify-center">
+        <Link to="/user-information" className="mr-[245px]">
+          <div className="flex items-center h-[46px] rounded-l-[40px] rounded-r-[40px] border-solid border-[#E0E0E0] border-[1.5px]">
+            <div className="w-[30px] h-[30px] ml-2 mr-2 rounded-[15px] bg-hpGray flex items-center justify-center">
               <BsFillPersonFill color="#FFF" size="23px" />
             </div>
-            <div className="lg:h-[30px] mr-4 text-center whitespace-nowrap overflow-y-hidden overflow-x-auto">
-              <span className="text-xl lg:text-2xl text-hpBlack">
-                {userName}
-              </span>
+            <div className="h-[30px] mr-4 text-center whitespace-nowrap overflow-y-hidden overflow-x-auto">
+              <span className="text-2xl text-hpBlack">{userName}</span>
             </div>
           </div>
         </Link>
       </header>
-      <hr className="w-[428px] mx-auto lg:w-[1440px] lg:h-[0.5px] bg-hpGray" />
+      <hr className="w-[1440px] h-[0.5px] mx-auto bg-hpGray" />
     </div>
   );
 }
