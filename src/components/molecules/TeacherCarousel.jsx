e53c3888f7896bdd3c7ae146c2a2574307c47dc7
gomdarachi <=>
2024-05-04 15:00:52 +0900

    feat: TeacherCarousel 컴포넌트 구현

--- src/components/molecules/TeacherCarousel.jsx
+++ src/components/molecules/TeacherCarousel.jsx
@@ -1,7 +1,62 @@
 import React from 'react';
+import { BsFillTriangleFill } from 'react-icons/bs';
 
-function TeacherCarousel() {
-  return <div>TeacherCarousel</div>;
+function TeacherCarousel({
+  teacherList,
+  selectedTeacherindex,
+  setSelectedTeacherindexIndex,
+}) {
+  const handleChangeSelectedTeacherindex = (flag) => {
+    if (flag === 'left') {
+      if (selectedTeacherindex === 0) {
+        setSelectedTeacherindexIndex(teacherList.length - 1);
+      } else {
+        setSelectedTeacherindexIndex((prev) => prev - 1);
+      }
+    } else if (flag === 'right') {
+      if (selectedTeacherindex === teacherList.length - 1) {
+        setSelectedTeacherindexIndex(0);
+      } else {
+        setSelectedTeacherindexIndex((prev) => prev + 1);
+      }
+    }
+  };
+
+  return (
+    <div>
+      <div className="border-solid border-[1.5px] border-black flex items-center justify-between px-4 rounded-3xl w-[233px] h-[42px]">
+        <button
+          type="button"
+          aria-label="왼쪽 넘기기"
+          onClick={() => {
+            handleChangeSelectedTeacherindex('left');
+          }}
+        >
+          <BsFillTriangleFill
+            className="origin-center rotate-[270deg]"
+            size="20px"
+            color="#BCBCBC"
+          />
+        </button>
+        <span className="h-[42px] leading-[42px] text-lg font-bold overflow-hidden">
+          {teacherList[selectedTeacherindex]} 선생님
+        </span>
+        <button
+          type="button"
+          aria-label="왼쪽 넘기기"
+          onClick={() => {
+            handleChangeSelectedTeacherindex('right');
+          }}
+        >
+          <BsFillTriangleFill
+            className="origin-center rotate-[90deg]"
+            size="20px"
+            color="#BCBCBC"
+          />
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default TeacherCarousel;
--- src/components/pages/WriteQueryPage.jsx
+++ src/components/pages/WriteQueryPage.jsx
@@ -2,13 +2,22 @@ import { useState } from 'react';
 import { BsFillPencilFill } from 'react-icons/bs';
 import InputImageButton from '../atoms/InputImageButton';
 import IconButton from '../atoms/IconButton';
+import TeacherCarousel from '../molecules/TeacherCarousel';
 
 function WriteQueryPage() {
   const [, setImgFiles] = useState([]);
   const finishWrite = () => {};
 
+  const teacherList = ['권나희', '하경현', '정은채'];
+  const [selectedTeacherindex, setSelectedTeacherindexIndex] = useState(0);
+
   return (
     <div className="w-full">
+      <TeacherCarousel
+        teacherList={teacherList}
+        selectedTeacherindex={selectedTeacherindex}
+        setSelectedTeacherindexIndex={setSelectedTeacherindexIndex}
+      />
       <div className="w-full absolute bottom-4">
         <div className="w-full flex justify-between px-12">
           <IconButton
