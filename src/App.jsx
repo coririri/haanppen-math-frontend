461f5af913a7055a7769edce8741cc81bf8fe3b5
gomdarachi <=>
2024-06-30 16:28:20 +0900

    feat: 학생 관리 페이지 api 기능 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import ReactModal from 'react-modal';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import LoginPage from './components/pages/LoginPage';
 import NonFoundPageServerError from './components/pages/NonFoundPageServerError';
 import ServerErrorPage from './components/pages/ServerErrorPage';
@@ -12,29 +13,32 @@ import QueryBoardPage from './components/pages/QueryBoardPage';
 import QuestionPage from './components/pages/QuestionPage';
 
 function App() {
+  const queryClient = new QueryClient();
   ReactModal.setAppElement('#root');
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* 공통 레이아웃 */}
-        <Route path="/" element={<CommonLayout />}>
-          <Route path="query-board" element={<QueryBoardPage />} />
-          <Route path="write-query" element={<WriteQueryPage />} />
-          <Route path="management" element={<ManagementPage />} />
-          <Route path="user-information" element={<UserInformationPage />} />
-          <Route path="question/:id" element={<QuestionPage />} />
-        </Route>
-        {/* 404 에러 */}
-        <Route path="*" element={<NonFoundClientError />} />
-        {/* 단독 레이아웃 */}
-        <Route path="/login" element={<LoginPage />} />
-        <Route
-          path="/nonfound-pageserver"
-          element={<NonFoundPageServerError />}
-        />
-        <Route path="server-error" element={<ServerErrorPage />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          {/* 공통 레이아웃 */}
+          <Route path="/" element={<CommonLayout />}>
+            <Route path="query-board" element={<QueryBoardPage />} />
+            <Route path="write-query" element={<WriteQueryPage />} />
+            <Route path="management" element={<ManagementPage />} />
+            <Route path="user-information" element={<UserInformationPage />} />
+            <Route path="question/:id" element={<QuestionPage />} />
+          </Route>
+          {/* 404 에러 */}
+          <Route path="*" element={<NonFoundClientError />} />
+          {/* 단독 레이아웃 */}
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/nonfound-pageserver"
+            element={<NonFoundPageServerError />}
+          />
+          <Route path="server-error" element={<ServerErrorPage />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
--- src/apis/student.js
+++ src/apis/student.js
@@ -1,6 +1,111 @@
 import instance from './instance';
 
-const studentAccountRegist = (setEnrollmentModalOpen, payload) => {
+export const getStudentList = ({ queryKey, pageParam, name = '' }) => {
+  console.log(queryKey[1]);
+  if (queryKey[1][0] === true)
+    return instance.get('/api/members/students', {
+      params: {
+        size: 10,
+        cursorIndex: pageParam,
+        startGrade: 0,
+        endGrade: 11,
+        name,
+      },
+    });
+
+  if (queryKey[1][1] === true)
+    return instance.get('/api/members/students', {
+      params: {
+        size: 10,
+        cursorIndex: pageParam,
+        startGrade: 0,
+        endGrade: 5,
+        name,
+      },
+    });
+
+  if (queryKey[1][2] === true)
+    return instance.get('/api/members/students', {
+      params: {
+        size: 10,
+        cursorIndex: pageParam,
+        startGrade: 6,
+        endGrade: 8,
+        name,
+      },
+    });
+
+  if (queryKey[1][3] === true)
+    return instance.get('/api/members/students', {
+      params: {
+        size: 10,
+        cursorIndex: pageParam,
+        startGrade: 9,
+        endGrade: 11,
+        name,
+      },
+    });
+  return instance.get('/api/members/students', {
+    params: {
+      size: 10,
+      cursorIndex: pageParam,
+      startGrade: 0,
+      endGrade: 16,
+      name: queryKey[2],
+    },
+  });
+};
+
+export const deleteStudent = (forDeletedStudentIds) =>
+  instance
+    .delete('api/accounts', {
+      data: {
+        targetIds: forDeletedStudentIds,
+      },
+    })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+export const modifyStudent = (
+  setEnrollmentModalOpen,
+  payload,
+  queryClient,
+  choosenGradeIndex,
+  searchNameValue,
+) =>
+  instance
+    .put('api/accounts/student', {
+      data: {
+        studentId: payload.id,
+        name: payload.name,
+        phoneNumber: payload.phoneNumber,
+        grade: payload.modificationGrade,
+      },
+    })
+    .then((response) => {
+      queryClient.invalidateQueries([
+        'studentList',
+        choosenGradeIndex,
+        searchNameValue,
+      ]);
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+const studentAccountRegist = (
+  setEnrollmentModalOpen,
+  payload,
+  queryClient,
+  choosenGradeIndex,
+  searchNameValue,
+) => {
+  console.log(choosenGradeIndex);
   instance
     .post('/api/accounts', {
       name: payload.name,
@@ -12,6 +117,12 @@ const studentAccountRegist = (setEnrollmentModalOpen, payload) => {
     .then((response) => {
       console.log(response);
       setEnrollmentModalOpen(false);
+
+      queryClient.invalidateQueries([
+        'studentList',
+        choosenGradeIndex,
+        searchNameValue,
+      ]);
     })
     .catch((error) => {
       console.log(error);
