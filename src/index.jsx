9abf33d907c04b8238a0cf86e3595bb306dc254e
gomDarachi <ttcoristory@naver.com>
2025-01-17 17:02:09 +0900

    fix: service-worker 대기 없앤

--- src/index.jsx
+++ src/index.jsx
@@ -15,7 +15,34 @@ reportWebVitals();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+// serviceWorkerRegistration.register();
+
+// 대기 중인 서비스 워커가 있다면 메시지 보내기
+if (navigator.serviceWorker) {
+  navigator.serviceWorker
+    .register('/service-worker.js')
+    .then((registration) => {
+      // 대기 중인 서비스 워커가 있는지 확인
+      if (registration.waiting) {
+        registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+      }
+
+      // 새 서비스 워커가 설치될 때마다 처리 (업데이트 발견 시)
+      registration.addEventListener('updatefound', () => {
+        const newWorker = registration.installing;
+        if (newWorker) {
+          newWorker.addEventListener('statechange', () => {
+            if (newWorker.state === 'installed' && registration.waiting) {
+              registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+            }
+          });
+        }
+      });
+    })
+    .catch((error) => {
+      console.error('Service Worker registration failed:', error);
+    });
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
