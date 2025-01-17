1ad3cbfe192598453535adb35bc1ad37117c5954
gomDarachi <ttcoristory@naver.com>
2025-01-17 17:55:58 +0900

    fix: servicw worker 해결

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -75,6 +75,7 @@ function registerValidSW(swUrl, config) {
                 'New content is available and will be used when all ' +
                   'tabs for this page are closed. See https://cra.link/PWA.',
               );
+              registration.waiting.postMessage({ type: 'SKIP_WAITING' });
 
               // Execute callback
               if (config && config.onUpdate) {
